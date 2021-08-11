1b3d596e753e67d111c4a4b1255cb7aaa8bfdead
Bart Dorsey <bart@bartdorsey.com>
2021-08-11 17:25:21 -0500

    Checking for WSL2 in a better way.

--- helpers/wsl2-checker.sh
+++ helpers/wsl2-checker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./helpers/colors.sh
 WSL=$(which wsl.exe)
-WSL_VERSION=$(wsl.exe -l -v | tr -d '\0' | grep Ubuntu | awk '{print $4}' | tr -d '\n\r')
+WSL_VERSION=$(wsl.exe -l -v | tr -d '\0' | grep Ubuntu | grep -c -E '\s2\s')
 
 hr
 title "Checking WSL"
