7ce3553d64582bc667c7abacbdf1e7f9795ec6b3
Bart Dorsey <bart@bartdorsey.com>
2021-09-03 09:29:19 -0500

    Updating wsl2 checker

--- helpers/wsl2-checker.sh
+++ helpers/wsl2-checker.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 source ./helpers/colors.sh
 WSL=$(which wsl.exe)
-WSL_VERSION=$(wsl.exe -l -v | tr -d '\0' | grep Ubuntu | awk '{print $3}' | head -n1 | tr -d '\n\r')
+WINDOWS_NAME=$(systeminfo.exe | findstr.exe /B /C:"OS Name")
+WINDOWS_VERSION=$(systeminfo.exe | findstr.exe /B /C:"OS Version")
+WINDOWS_BUILD=$(echo $WINDOWS_VERSION | tr -d '\0\r' | awk '{print $6}')
+WSL_VERSION=$(wsl.exe --status | tr -d '\0\r'|grep "Default Version"| awk '{print $3}')
+
+MINIMUM_WINDOWS_BUILD_VERSION=19043
 
 hr
 title "Checking WSL"
 hr
+echo $WINDOWS_NAME
+echo $WINDOWS_VERSION
+echo "Windows Build: $WINDOWS_BUILD"
 echo "WSL Version: $WSL_VERSION"
+
+if [ $WINDOWS_BUILD -lt $MINIMUM_WINDOWS_BUILD_VERSION ]; then
+    c_red "Your Windows 10 build version isn't high enough"
+    c_red "Please run Windows update and update to at least build $MINIMUM_WINDOWS_BUILD_VERSION"
+    exit 1;
+fi
 
 if [ -z "$WSL" ]; then
     c_red "WSL doesn't appear to be installed."
